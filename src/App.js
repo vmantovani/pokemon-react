1a8fad0e53f01669b5860a4208b081e3a3ec8905
Vinicius Mantovani <vmantovani@gmail.com>
2022-10-26 20:46:36 +0200

    Adicionado o Styled Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import Pages from './pages/Pages';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      <Pages />
+      <Container>
+        <Pages />
+      </Container>
     </div>
   );
 }
+
+const Container = styled.div`
+  width: 90%;
+  margin: auto;
+  padding-top: 8rem;
+  display: flex;
+`;
 
 export default App;
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 
 function PokemonList() {
+  const [pokemons, setPokemons] = React.useState([]);
+
   const getPokemon = async () => {
-    const api = await fetch(`https://pokeapi.co/api/v2/pokemon/`);
+    const api = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=1150`);
     const data = await api.json();
     console.log(data);
+    setPokemons(data.results);
   };
 
   React.useEffect(() => {
     getPokemon();
   }, []);
 
-  return <div>PokemonList</div>;
+  return (
+    <div>
+      {pokemons.map((pokemon) => (
+        <li key={pokemon.name}>{pokemon.name}</li>
+      ))}
+    </div>
+  );
 }
 
 export default PokemonList;
